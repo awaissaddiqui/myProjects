d003c28ca656784ad9dce53a836bed9e80c33e29
awais <awaissaddiqui143@gmail.com>
2022-04-09 16:58:16 +0500

    new

--- file.js
+++ file.js
@@ -1,21 +1,21 @@
-const data = [
-    {
-        name: "Laptop",
-        price: 1000,
-        isAvailable: true
-    }, 
-    {
-        name: "Mobile",
-        price: 8000,           //randomuser.me/portrait/men
-        isAvailable: true                      //JavaScript Object Notation (JSON) is a standard text-based format for representing structured data based on JavaScript object syntax. It is commonly used for transmitting data in web applications (e.g., sending some data from the server to the client, so it can be displayed on a web page, or vice versa)                              
+// const data = [
+//     {
+//         name: "Laptop",
+//         price: 1000,
+//         isAvailable: true
+//     }, 
+//     {
+//         name: "Mobile",
+//         price: 8000,           //randomuser.me/portrait/men
+//         isAvailable: true                      //JavaScript Object Notation (JSON) is a standard text-based format for representing structured data based on JavaScript object syntax. It is commonly used for transmitting data in web applications (e.g., sending some data from the server to the client, so it can be displayed on a web page, or vice versa)                              
         
-    },
-    {
-        name: "TV",
-        price: 9000,
-        isAvailable: true
-    }
-];
+//     },
+//     {
+//         name: "TV",
+//         price: 9000,
+//         isAvailable: true
+//     }
+// ];
 
 /*for(let i = 0; i< data.length; i++) {
     console.log(data[i].name)      //nodemon (file name)
@@ -28,16 +28,34 @@ for (let i = 0; i < data.length; i++) {
     }
     console.log(sum)*/
                                         //par
-                                        for Application Programming Interface
-}
-var sale;
-for (let i = 0; i < data.length; i++) {
-    if(data[i].isAvailable) {
-      sale = data[i].price - (20*data[i].price/100);  //bracket pair colorize (extension)
-      data[i].price = sale;
-    }
-}
-    console.log(data[i].price)
+                                       // for Application Programming Interface
+//}
+// var sale;
+// for (let i = 0; i < data.length; i++) {
+//     if(data[i].isAvailable) {
+//       sale = data[i].price - (20*data[i].price/100);  //bracket pair colorize (extension)
+//       data[i].price = sale;
+//     }
+// }
+//     console.log(data[i].price)
 
 
+
+/*New work of Javascript*/
+
+const marks = [45, 33, 20, 77, 80, 99, 12, 34, 56, 78,];
+
+// const sum = marks
+//     .sort((a,b) => a - b)
+//     .filter((n) => n >50)
+//     .map((a) => a + 10)
+//     .reduce((a,b) => a + b, 0);
+   
+    const l = marks.indexOf(20)
+    // if(l == -1){
+    //     marks.push(100)
+    // }
+    console.log(l)
+
+  //  console.log(sum)
     
