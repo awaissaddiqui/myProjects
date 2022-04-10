f00e1201796db3e4ad089c2d433a39cf32cb0d7d
awais <awaissaddiqui143@gmail.com>
2022-04-10 06:41:46 +0500

    new

--- file.js
+++ file.js
@@ -18,7 +18,8 @@
 // ];
 
 /*for(let i = 0; i< data.length; i++) {
-    console.log(data[i].name)      //nodemon (file name)
+    console.log(data[i].name)     
+                              //nodemon (file name)
 }
 var sum = 0
 for (let i = 0; i < data.length; i++) {
@@ -27,8 +28,8 @@ for (let i = 0; i < data.length; i++) {
         console.log(data[i].price)
     }
     console.log(sum)*/
-                                        //par
-                                       // for Application Programming Interface
+                                        //parse
+                        // API for Application Programming Interface
 //}
 // var sale;
 // for (let i = 0; i < data.length; i++) {
@@ -55,5 +56,62 @@ const add = [33, 44,]
  // ( marks.indexOf(45) == -1) ? marks.push(100) : marks.pop();
   
    //  console.log(sum)
-    const full = marks.concat(add);  //concat mean to add element of one aray to other with ends.
-    console.log(full)
+    // const full = marks.concat(add);  //concat mean to add element of one aray to other with ends.
+    // console.log(full)
+
+    // list = []
+    // list.fill(10,0,10)
+
+    //Destructring 
+          // desturcturing have two parts , one is objct destructuring and other is ary.
+          // in aray desturctring we can't change indexes.
+          
+
+    // const names = ["khan" , "Awais", 
+    // "Taheed", "Ali"];
+    // const [name1,,name2] = names; 
+    // console.log(name1)
+
+    // const student_list = {
+    //   name: 'Awais',
+    //   age:23,
+    //   marks: 30,
+    //   isPass: true,
+    //   friends:["khan" , "Awais", 
+    //    "Taheed", "Ali"],
+    //    address: {
+    //      city: 'mardan',
+    //      village: 'sarhad'
+    //    }
+    // }
+    // const {name, address} = student_list;
+    // console.log(address.city)
+
+  //   changeMarks(student_list);
+
+  //   const changeMarks = (marks, address) => {
+  //   obj.marks = 50
+  // }
+
+      /*String */
+   const str1 = "Hello";
+   const str2 = " world ";
+   //const str = "This is a new line.";
+   //const str3 = str1.concat(str2)
+  const str = "This is a new Pakistan";
+   //const str3 = str1 + str2;
+   //const str4 = $(str1) $(str2) ;
+  // console.log(str3)
+
+  const l = str.indexOf("line")
+  //console.log(l)
+const l2 = str.replace("new", "old");
+//console.log(l2)
+/*SLICE */
+// The extraction of things from one place to anther  from string is slice.
+//  const country = str.slice(str.indexOf("key"),str.indexOf(key) + key.length)
+//  console.log(country)
+
+const string = "1, Faizan, mardan, 3.5, 6th, uet"
+
+const [id, name, address, gpa, semester, institue] = string.split(",") ;
